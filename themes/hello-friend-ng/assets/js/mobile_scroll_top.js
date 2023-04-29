1db2e11dfaaee9749434fbdaab34e3e73ed63a5b
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2023-04-29 18:23:59 +0200

    Add mobile scrolling button.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

---
+++ themes/hello-friend-ng/assets/js/mobile_scroll_top.js
@@ -0,0 +1,28 @@
+// Arrow that scrolls to top on mobile devices
+var target = document.querySelector("post-title");
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function callback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      scrollToTopBtn.classList.remove("showBtn");
+    } else {
+      scrollToTopBtn.classList.add("showBtn");
+    }
+  });
+}
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+let observer = new IntersectionObserver(callback);
+if (isMobile()) {
+    observer.observe(target);
+}
